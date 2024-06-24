62b0b0663d6fc4616d0e6be1ad0be544d73dc63d
Artilleryxd <karandwari@gmail.com>
2024-06-24 22:29:19 +0530

    Added Marquee

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import Marquee from 'react-fast-marquee';
+
 
 const Home = () => {
   return (
     <div>
         <Nav/>
-        <h1>Home page</h1>
+        <Marquee pauseOnHover="true" className='bg-cyan-600 h-10'>
+          <div>
+            <h1 className='text-2xl text-white'> #CapTech2024- “Discover. Connect. Execute”- Monday 25th November 2024 to Thursday 28th November 2024- ICC Sydney Australia</h1>
+          </div>
+        </Marquee>
     </div>
   )
 }
