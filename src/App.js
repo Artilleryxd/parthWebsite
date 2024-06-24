e1093c7c5207244dad86d31f06c3563a79add6ca
Artilleryxd <karandwari@gmail.com>
2024-06-24 21:54:34 +0530

    Created routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import Nav from './components/Nav'
+import Nav from './components/Nav';
+import {BrowserRouter , Routes ,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 const App = () => {
   return (
     <div>
-      <Nav/>
+      <BrowserRouter>
+          <Routes>
+              <Route index element={<Home/>}/>
+              <Route path='/home' element={<Home/>}/>
+              <Route path='/about' element={<About/>}/>
+              <Route path='/contact' element={<Contact/>}/>
+          </Routes>
+      </BrowserRouter>
     </div>
   )
 }
